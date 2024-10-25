932bee9a2bdfe30443eeaa27a8a30547d0c05df5
marioamado-code <mario.amado@iesnervion.es>
2024-10-25 17:53:30 +0200

    ejercicio 2 parte 7

--- Tema2/src/parte7/Ejercicio2.java
+++ Tema2/src/parte7/Ejercicio2.java
@@ -9,12 +9,11 @@ public class Ejercicio2 {
 
 		int num;
 
-		// Hay que crear un booleano que sirva para salir del bucle
+		// Hay que crear un booleano que sirva para salir del bucle for del mismo número
 
 		boolean salir = false;
 
 		int cont = 0;
-		
 
 		// Introducimos el escáner
 
@@ -33,9 +32,12 @@ public class Ejercicio2 {
 			// Si no es positivo pedimos que lo vuelva a introducir mediante un do while
 
 		} while (num <= 0);
-
-		for (int j = 2; j <=num; j++) {
-			salir=false;
+		
+		//Creamos un bucle for para cada número desde el 2 hasta el número en cuestión
+		
+		for (int j = 2; j <= num; j++) {
+			//Indicamos que salir es false para que cuando pase por el true luego se reinicie
+			salir = false;
 			// Introducimos el bucle for para ver si el resto del numero entre cualquier
 
 			// otro número menor es 0, empezamos por su raiz y vamos bajando para encontrar
@@ -49,18 +51,15 @@ public class Ejercicio2 {
 				// Abrimos un ternario para cada número y si es true se sale del bucle
 
 				salir = j % i == 0 ? true : false;
-				
 
-			
-
-			
 			}
+			//Si salir es false quiere decir que el número es primo y se le añade al contador
 			if (salir == false) {
 
 				cont++;
+			}
 		}
-		}
-		
+
 		// Imprimimos el número de números primos
 
 		System.out.println("Hay " + cont + " números primos entre 1 y " + num);
